8e8b501107e503e5cc5974ad68a50de364fdca7f
Victor Benavente <vbenavente@hotmail.com>
2017-03-28 09:50:45 -0400

    non alpha and non digits are preserved

--- src/test_play_passphrase.py
+++ src/test_play_passphrase.py
@@ -2,10 +2,18 @@ from play_passphrase import play_pass
 
 
 def test_same_phrase_is_not_returned():
+    """Test phrase is changed."""
     result = play_pass("I love python 8765", 2)
     assert result != "I love python 8765"
 
 
 def test_alpha_shift_works():
+    """Test alpha shift is working."""
     result = play_pass("i love python 3 4", 3)
     assert result == "l oryh sbwkrq 6 5"
+
+
+def test_non_alpha_digit_char_kept():
+    """Test non alpha or digit characters are kept."""
+    result = play_pass("a!! b@@ c##", 1)
+    assert result == "b!! c@@ d##"
