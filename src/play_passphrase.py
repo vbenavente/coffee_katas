4a000684c550ec9be28dc80dabbcc7a42e61772d
Victor Benavente <vbenavente@hotmail.com>
2017-03-28 10:11:41 -0400

    case change based on position working!

--- src/play_passphrase.py
+++ src/play_passphrase.py
@@ -1,6 +1,7 @@
 def play_pass(s, n):
     alpha = 'abcdefghijklmnopqrstuvwxyz'
     new_s = ''
+    counter = 0
     for item in s:
         if item.lower() in alpha:
             idx = alpha.find(item)
@@ -9,11 +10,15 @@ def play_pass(s, n):
             else:
                 idx = (idx + n) - 26
             item = alpha[idx]
-            new_s += item
+            if counter % 2 == 0:
+                item = item.upper()
+            else:
+                item = item.lower()
         else:
             try:
                 item = 9 - int(item)
-                new_s += str(item)
             except ValueError:
-                new_s += item
+                item = item
+        new_s += str(item)
+        counter += 1
     return new_s
--- src/test_play_passphrase.py
+++ src/test_play_passphrase.py
@@ -10,10 +10,16 @@ def test_same_phrase_is_not_returned():
 def test_alpha_shift_works():
     """Test alpha shift is working."""
     result = play_pass("i love python 3 4", 3)
-    assert result == "l oryh sbwkrq 6 5"
+    assert result == "L OrYh sBwKrQ 6 5"
 
 
 def test_non_alpha_digit_char_kept():
     """Test non alpha or digit characters are kept."""
     result = play_pass("a!! b@@ c##", 1)
-    assert result == "b!! c@@ d##"
+    assert result == "B!! C@@ D##"
+
+
+def test_upcase_even_downcase_odd():
+    """Test letter in odd position downcase, even upcase."""
+    result = play_pass("even odd", 0)
+    assert result == "EvEn oDd"
